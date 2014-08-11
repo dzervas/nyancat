392c318af72085e2308a74fe94f2945edd873519
Dimitris Zervas <dzervas@dzervas.gr>
2014-08-11 03:03:36 +0300

    Cleaned the code a bit

--- main.c
+++ main.c
@@ -1,24 +1,13 @@
-#include <stdint.h>
 #include <avr/io.h>
-//#include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include "nyan.h"
-
-#define SAMPLE_RATE 8000;
-#define outLow(port,pin) port &= ~(1 << pin)
-#define outHigh(port,pin) port |= (1 << pin)
-#define outToggle(port,pin) port ^= (1 << pin)
-#define setIn(portdir,pin) portdir &= ~(1 << pin)
-#define setOut(portdir,pin) portdir |= (1 << pin)
 
 volatile uint16_t sample;
 
 int main(void) {
 	sample = 0;
 
-	setOut(DDRD, 3); // Speaker
-	setOut(DDRD, 7);
-	outHigh(PORTD, 7);
+	DDRD |= (1 << 3); // Speaker
 
 	/* Set clk/8 prescalar for the counter0 */
 	TCCR0B |= (1 << CS01);
@@ -29,7 +18,6 @@ int main(void) {
 	while (1) {
 
 		if (TCNT0 >= 249) {
-			outLow(PORTD, 7);
 			OCR2B = pgm_read_byte(&pcm_data[sample++]);
 			TCNT0 = 0;
 		}
